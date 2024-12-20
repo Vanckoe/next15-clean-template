0edb11b44286ff60def234bd1062839566d851f1
Arsik0153 <arsik0153@gmail.com>
2024-12-20 18:16:45 +0900

    feature: configure svgr

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,37 @@ const nextConfig: NextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    webpack(config) {
+        // @ts-expect-error https://react-svgr.com/docs/next/#usage
+        const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+
+        config.module.rules.push(
+            {
+                ...fileLoaderRule,
+                test: /\.svg$/i,
+                resourceQuery: /url/, // *.svg?url
+            },
+            {
+                test: /\.svg$/i,
+                issuer: fileLoaderRule.issuer,
+                resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+                use: ['@svgr/webpack'],
+            },
+        );
+        fileLoaderRule.exclude = /\.svg$/i;
+
+        return config;
+    },
+    experimental: {
+        turbo: {
+            rules: {
+                '*.svg': {
+                    loaders: ['@svgr/webpack'],
+                    as: '*.js',
+                },
+            },
+        },
+    },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import React from 'react';
+import FileIcon from '@/assets/icons/file.svg';
+
 export default function Home() {
     return (
         <div>
-            <h1 className="text-2xl">Empty project</h1>
+            <h1 className="text-2xl">
+                Empty project
+                <FileIcon width={150} height={150} color="red" />
+            </h1>
         </div>
     );
 }
